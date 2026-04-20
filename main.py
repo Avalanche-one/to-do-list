3f7d40384f7b78258c93ba6877635d06374503d3
avalanche <avalanche3122@gmail.com>
2026-04-20 20:26:27 +0300

    initial commit

---
+++ main.py
@@ -0,0 +1,8 @@
+def show_menu():
+    title = "To Do List"
+    menu_options = {1: "Show List", 2: "Add Item", 3: "Remove Item", 4: "Quit"}
+    print("=" * 20)
+    print(title)
+    print("=" * 20)
+    for key, value in menu_options.items():
+        print(f"{key}: {value}")
