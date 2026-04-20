f7edba88de4afee0564bbfb48441b85835ffbbf7
avalanche <avalanche3122@gmail.com>
2026-04-20 20:34:00 +0300

    added main cycle and comments

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+#menu function
 def show_menu():
     title = "To Do List"
     menu_options = {1: "Show List", 2: "Add Item", 3: "Remove Item", 4: "Quit"}
@@ -6,3 +7,10 @@ def show_menu():
     print("=" * 20)
     for key, value in menu_options.items():
         print(f"{key}: {value}")
+#main cycle
+while True:
+    show_menu()
+    choice = int(input("Enter your choice: "))
+    print(f"You've chosen {choice}")
+    if choice == 4:
+        break
